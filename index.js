ceaff5db03a4f188456b120cc97eeae4e36964ef
JohnnyScript <iroelejohnny@gmail.com>
2024-05-21 10:42:53 +0100

    fixed the cors issue again

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ const io = new Server(server, {
 });
 
 //MIDDLEWARE
-app.use(cors("*"));
+app.use(
+  cors({
+    origin: process.env.DOMAIN,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(helmet());
