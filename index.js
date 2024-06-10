71932d64dab65b49d3a19367db1a2f3b69bcf919
Johnny <110586185+JonR-lang@users.noreply.github.com>
2024-06-10 11:08:48 +0100

    Update index.js
    Added a new domain for cors to accept temporarily

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
     credentials: true,
   },
 });
